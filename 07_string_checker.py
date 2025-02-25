ac08c64b895d84dbcdd9375316300d1b929385cf
TopazSkylar <138179868+TopazSkylar@users.noreply.github.com>
2025-02-26 09:54:08 +1300

    Add files via upload

--- 07_string_checker.py
+++ 07_string_checker.py
@@ -1,5 +1,6 @@
 # function goes here
-def string_checker(question, valid_ans_list=('yes', 'no'), num_letters=1):
+# checks that users enter a valid response (yes/no, cash/credit) based on a list of options
+def string_checker(question, valid_ans_list, num_letters=1):
     """Checks that users enter the full word or then, letter/s of a word from a list of valid responses"""
 
     while True:
@@ -17,14 +18,16 @@ def string_checker(question, valid_ans_list=('yes', 'no'), num_letters=1):
             elif response == item[:num_letters]:
                 return item
 
+        print(f"Please enter {valid_ans_list})")
+
 
 # main routine goes here
 payment_list = ["cash", "credit"]
-
+yes_no_list = ["yes", "no"]
 for case in range(0,5):
-    want_instructions = string_checker("Do you want to read the instructions? ")
+    want_instructions = string_checker("Do you want to read the instructions? ", yes_no_list)
     print(f"You chose {want_instructions}")
 
 for case in range(0,5):
-    pay_method = string_checker("Choose a payment method (cash/credit)", 2)
+    pay_method = string_checker("Choose a payment method (cash/credit)", payment_list, 2)
     print(f"You chose {pay_method}")
